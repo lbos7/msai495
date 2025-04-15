22e89548c8906367a6b779f2dd79d4aaaf055b69
Logan Boswell <loganstuartboswell@gmail.com>
2025-04-15 16:30:57 -0500

    Added morph operator definitions

---
+++ mp2/MorphologicalOperators.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np
+
+def Erosion(img, se):
+    return
+
+def Dilation(img, se):
+    return
+
+def Opening(img, se):
+    return
+
+def Closing(img, se):
+    return
+
+def Boundary(img, se):
+    return
