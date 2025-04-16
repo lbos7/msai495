db23271c6cfae0e3cf4b496c175492620f908ec5
Logan Boswell <loganstuartboswell@gmail.com>
2025-04-15 23:06:28 -0500

    Added implementation file and got dilation to work

--- mp2/MorphologicalOperators.py
+++ mp2/MorphologicalOperators.py
@@ -1,10 +1,29 @@
 import cv2
 import numpy as np
 
-def Erosion(img, se):
+def Dilation(img, se):
+    
+    img_gray = np.copy(img)
+    img_gray = cv2.cvtColor(img_gray, cv2.COLOR_BGR2GRAY)
+    rows,cols = img_gray.shape
+
+    se_offset_rows = se[0] // 2
+    se_offset_cols = se[1] // 2
+
+    white_pix_coords = np.argwhere(img_gray == 255)
+
+    for coord in white_pix_coords:
+        i = coord[0]
+        j = coord[1]
+        for row in np.clip(np.linspace(i - se_offset_rows, i + se_offset_rows, se[0], dtype=int), 0, rows - 1):
+                for col in np.clip(np.linspace(j - se_offset_cols, j + se_offset_cols, se[1], dtype=int), 0, cols - 1):
+                    img_gray[row, col] = 255
+    return img_gray
+
+
     return
 
-def Dilation(img, se):
+def Erosion(img, se):
     return
 
 def Opening(img, se):
---
+++ mp2/mp2_implementation.py
@@ -0,0 +1,13 @@
+import cv2
+import numpy as np
+from MorphologicalOperators import Dilation
+
+gun_img = cv2.imread('mp2/images/gun.bmp')
+
+se = (3, 3)
+gun_img_dilated = Dilation(gun_img, se)
+
+cv2.imshow('gun.bmp', gun_img)
+cv2.imshow('dilated gun.bmp', gun_img_dilated)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
