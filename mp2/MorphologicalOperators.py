44908e991b17b778799c668c92ce96eb477e7d00
Logan Boswell <loganstuartboswell@gmail.com>
2025-04-16 00:09:43 -0500

    Updated Erosion function - runs quicker

--- mp2/MorphologicalOperators.py
+++ mp2/MorphologicalOperators.py
@@ -25,10 +25,11 @@ def Erosion(img, se):
     img_gray = cv2.cvtColor(img_gray, cv2.COLOR_BGR2GRAY)
     rows,cols = img_gray.shape
 
+    img_new = np.copy(img_gray)
+    img_new[:, :] = 0
+
     se_offset_rows = se[0] // 2
     se_offset_cols = se[1] // 2
-
-    keep_coords = []
 
     white_pix_coords = np.argwhere(img_gray == 255)
 
@@ -40,17 +41,9 @@ def Erosion(img, se):
             for col in np.unique(np.clip(np.linspace(j - se_offset_cols, j + se_offset_cols, se[1], dtype=int), 0, cols - 1)):
                 sum += img_gray[row, col]
         if sum == 255*se[0]*se[1]:
-            keep_coords.append(coord)
+            img_new[i, j] = 255
 
-    for coord in white_pix_coords:
-        keep = False
-        for keep_coord in keep_coords:
-            if np.array_equal(coord, keep_coord):
-                keep = True
-                break
-        if not keep:
-            img_gray[coord[0], coord[1]] = 0
-    return cv2.cvtColor(img_gray, cv2.COLOR_GRAY2BGR)
+    return cv2.cvtColor(img_new, cv2.COLOR_GRAY2BGR)
 
 def Opening(img, se):
     eroded_img = Erosion(img, se)
