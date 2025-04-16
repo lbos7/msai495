5234e9cbd0ecbed7757e1898cf0a53223df447e0
Logan Boswell <loganstuartboswell@gmail.com>
2025-04-15 23:45:35 -0500

    Got erosion to work

--- mp2/MorphologicalOperators.py
+++ mp2/MorphologicalOperators.py
@@ -15,16 +15,42 @@ def Dilation(img, se):
     for coord in white_pix_coords:
         i = coord[0]
         j = coord[1]
-        for row in np.clip(np.linspace(i - se_offset_rows, i + se_offset_rows, se[0], dtype=int), 0, rows - 1):
-                for col in np.clip(np.linspace(j - se_offset_cols, j + se_offset_cols, se[1], dtype=int), 0, cols - 1):
+        for row in np.unique(np.clip(np.linspace(i - se_offset_rows, i + se_offset_rows, se[0], dtype=int), 0, rows - 1)):
+                for col in np.unique(np.clip(np.linspace(j - se_offset_cols, j + se_offset_cols, se[1], dtype=int), 0, cols - 1)):
                     img_gray[row, col] = 255
-    return img_gray
-
-
-    return
+    return cv2.cvtColor(img_gray, cv2.COLOR_GRAY2BGR)
 
 def Erosion(img, se):
-    return
+    img_gray = np.copy(img)
+    img_gray = cv2.cvtColor(img_gray, cv2.COLOR_BGR2GRAY)
+    rows,cols = img_gray.shape
+
+    se_offset_rows = se[0] // 2
+    se_offset_cols = se[1] // 2
+
+    keep_coords = []
+
+    white_pix_coords = np.argwhere(img_gray == 255)
+
+    for coord in white_pix_coords:
+        i = coord[0]
+        j = coord[1]
+        sum = 0
+        for row in np.unique(np.clip(np.linspace(i - se_offset_rows, i + se_offset_rows, se[0], dtype=int), 0, rows - 1)):
+            for col in np.unique(np.clip(np.linspace(j - se_offset_cols, j + se_offset_cols, se[1], dtype=int), 0, cols - 1)):
+                sum += img_gray[row, col]
+        if sum == 255*se[0]*se[1]:
+            keep_coords.append(coord)
+
+    for coord in white_pix_coords:
+        keep = False
+        for keep_coord in keep_coords:
+            if np.array_equal(coord, keep_coord):
+                keep = True
+                break
+        if not keep:
+            img_gray[coord[0], coord[1]] = 0
+    return cv2.cvtColor(img_gray, cv2.COLOR_GRAY2BGR)
 
 def Opening(img, se):
     return
--- mp2/mp2_implementation.py
+++ mp2/mp2_implementation.py
@@ -1,13 +1,15 @@
 import cv2
 import numpy as np
-from MorphologicalOperators import Dilation
+from MorphologicalOperators import Dilation, Erosion
 
 gun_img = cv2.imread('mp2/images/gun.bmp')
 
 se = (3, 3)
 gun_img_dilated = Dilation(gun_img, se)
+gun_img_eroded = Erosion(gun_img, se)
 
 cv2.imshow('gun.bmp', gun_img)
 cv2.imshow('dilated gun.bmp', gun_img_dilated)
+cv2.imshow('eroded gun.bmp', gun_img_eroded)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
