ae5908cdf072870aafbae48afa4fe6b0629c8802
Logan Boswell <loganstuartboswell@gmail.com>
2025-04-17 23:54:59 -0500

    finished mp2

--- mp2/mp2_implementation.py
+++ mp2/mp2_implementation.py
@@ -5,18 +5,20 @@ from MorphologicalOperators import Dilation, Erosion, Opening, Closing, Boundary
 gun_img = cv2.imread('mp2/images/gun.bmp')
 palm_img = cv2.imread('mp2/images/palm.bmp')
 
-se = (3, 3)
+se = (7, 7)
+se2 = (3, 3)
 gun_img_dilated = Dilation(gun_img, se)
 gun_img_eroded = Erosion(gun_img, se)
 gun_img_opened = Opening(gun_img, se)
 gun_img_closed = Closing(gun_img, se)
-gun_img_boundary = Boundary(gun_img, se)
+gun_img_boundary = Boundary(gun_img_closed, se2)
 
 palm_img_dilated = Dilation(palm_img, se)
 palm_img_eroded = Erosion(palm_img, se)
 palm_img_opened = Opening(palm_img, se)
 palm_img_closed = Closing(palm_img, se)
-palm_img_boundary = Boundary(palm_img, se)
+palm_img_closed2 = Erosion(palm_img_dilated, se2)
+palm_img_boundary = Boundary(palm_img_closed2, se2)
 
 cv2.imshow('gun.bmp', gun_img)
 cv2.imshow('dilated gun.bmp', gun_img_dilated)
