3b101af2d0c2a27a0c5e809257109998f3bd7a1c
Logan Boswell <loganstuartboswell@gmail.com>
2025-04-21 21:59:03 -0500

    Added to plots and images directories; added comments

--- mp3/HistoEqualization.py
+++ mp3/HistoEqualization.py
@@ -1,28 +1,32 @@
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
 
 def HistoEqualization(img):
+    '''
+    Applies the histogram equalization to a given image.
+    
+    Parameters:
+        img (numpy array): The image the operation will be performed on.
+        
+    Returns:
+        (numpy array): The processed image.
+    '''
 
     # Converting image to grayscale and determining dimensions
     img_gray = np.copy(img)
     img_gray = cv2.cvtColor(img_gray, cv2.COLOR_BGR2GRAY)
     rows,cols = img_gray.shape
 
-    hist = np.histogram(img_gray.flatten(), bins=256, density=True)
+    # Generating histogram and cdf
+    hist,bins = np.histogram(img_gray.flatten(), bins=256, density=True)
     cdf = np.cumsum(hist)
 
+    # Defining array for transforming input and output gray levels
     L2_arr = cdf*255
     
+    # Editing pixel values based on transformation
     for i in range(rows):
         for j in range(cols):
             img_gray[i, j] = int(L2_arr[img_gray[i, j]])
 
     return cv2.cvtColor(img_gray, cv2.COLOR_GRAY2BGR)
-    # plt.figure(1)
-    # plt.plot(cdf * hist.max(), color='b')
-    # plt.hist(img_gray.flatten(), bins=256, density=True, color='r')
-    # # plt.stairs(hist, bins)
-    # plt.legend(['cdf (scaled)', 'histogram'])
-    # plt.title("moon.bmp histogram")
-    # plt.show()
--- mp3/mp3_implementation.py
+++ mp3/mp3_implementation.py
@@ -8,4 +8,5 @@ moon_he_img = HistoEqualization(moon_img)
 
 cv2.imshow('moon.bmp', moon_img)
 cv2.imshow('HistoEqualization on moon.bmp', moon_he_img)
+cv2.waitKey(0)
 cv2.destroyAllWindows()
