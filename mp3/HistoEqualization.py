d7802fc776627fb275266a7f594fbfb9d50e72c1
Logan Boswell <loganstuartboswell@gmail.com>
2025-04-21 21:36:15 -0500

    Got HistoEqualization to work

--- mp3/HistoEqualization.py
+++ mp3/HistoEqualization.py
@@ -9,10 +9,20 @@ def HistoEqualization(img):
     img_gray = cv2.cvtColor(img_gray, cv2.COLOR_BGR2GRAY)
     rows,cols = img_gray.shape
 
-    img_gray_1d = img_gray.flatten()
-    hist = np.histogram(img_gray_1d, bins=256)
+    hist = np.histogram(img_gray.flatten(), bins=256, density=True)
+    cdf = np.cumsum(hist)
+
+    L2_arr = cdf*255
     
-    plt.figure(1)
-    plt.hist(img_gray_1d, bins=256)
-    plt.title("moon.bmp histogram")
-    plt.show()
+    for i in range(rows):
+        for j in range(cols):
+            img_gray[i, j] = int(L2_arr[img_gray[i, j]])
+
+    return cv2.cvtColor(img_gray, cv2.COLOR_GRAY2BGR)
+    # plt.figure(1)
+    # plt.plot(cdf * hist.max(), color='b')
+    # plt.hist(img_gray.flatten(), bins=256, density=True, color='r')
+    # # plt.stairs(hist, bins)
+    # plt.legend(['cdf (scaled)', 'histogram'])
+    # plt.title("moon.bmp histogram")
+    # plt.show()
