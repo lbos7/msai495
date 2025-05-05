91d2b5a19053c7303e2611274dbd5c27dd72bbe4
Logan Boswell <loganstuartboswell@gmail.com>
2025-05-05 14:41:41 -0500

    Added comments to HoughTransform

--- mp6/HoughTransform.py
+++ mp6/HoughTransform.py
@@ -2,64 +2,83 @@ import cv2
 import numpy as np
 from CannyEdgeDetection import CannyEdgeDetection
 import matplotlib.pyplot as plt
+from scipy.ndimage import maximum_filter, label, center_of_mass
 
+def HoughTransform(img, param_quant=180, N=5, sigma=1, percentageOfNonEdge=.85, max_filt_N=15, peaks_thresh_mul=.5):
+    '''
+    Detects prominent lines in a given image.
+    
+    Parameters:
+        img (numpy array): The input image.
+        param_quant (int): The number of parameter test points.
+        N (int): The kernel size for smoothing.
+        sigma (int): The standard deviation for smoothing.
+        percentageOfNonEdge (float): Percentage of pixels that are not part of an edge.
+        max_filt_N (int): Neighborhodd size for maximum filter.
+        peaks_thresh_mul (float): Threshold multiplier for max filter peaks mask.
+        
+    Returns:
+        (numpy array): The input image with the detected prominent lines.
+    '''
 
-def HoughTransform(img, param_quant=180, N=5, sigma=1, percentageOfNonEdge=.85):
-
+    # Copying image, finding shape of imaage, and performing edge detection
     img_copy = np.copy(img)
     img_edges = CannyEdgeDetection(img, N=N, sigma=sigma, percentageOfNonEdge=percentageOfNonEdge)
     img_edges = cv2.cvtColor(img_edges, cv2.COLOR_BGR2GRAY)
-
     rows,cols = img_edges.shape
 
+    # Finding offsets for range of possible parameter values based on image size
     rho_offset = int((rows ** 2 + cols ** 2) ** .5)
     theta_offset = 90
+
+    # Setting up accumulator matrix
     accum_mat = np.zeros((2 * theta_offset + 1, 2 * rho_offset + 1))
     accum_mat = accum_mat.astype(np.int16)
 
+    # Generating list of edge pixel coordinates
     edge_coords = np.argwhere(img_edges == 255)
 
+    # Setting up list ot angles to check based on param_quant variable
     theta_check = np.linspace(-theta_offset, theta_offset, num=param_quant, dtype=np.int16)
 
+    # Mapping from row,col to theta,rho
     for coord in edge_coords:
         row = coord[0]
         col = coord[1]
 
+        # Calcutating rho using the current row,col coordinates with each angle in theta_check
         for theta in theta_check:
             rho = int(row * np.cos(np.deg2rad(theta)) + col * np.sin(np.deg2rad(theta)))
             accum_mat[theta + theta_offset, rho + rho_offset] += 1
 
     max_accum_val = np.max(accum_mat)
-    line_coords = np.argwhere(accum_mat > max_accum_val * .5)
 
-    for line in line_coords:
-        theta = line[0] - theta_offset
-        rho = line[1] - rho_offset
+    # Apply maximum filter to find local maxima
+    filtered = maximum_filter(accum_mat, size=max_filt_N)  # adjust size as needed
+    peaks_mask = (accum_mat == filtered) & (accum_mat > max_accum_val*peaks_thresh_mul)
 
+    # Label and get center of each peak
+    labeled,_ = label(peaks_mask)
+    peaks = center_of_mass(accum_mat, labeled, range(1, labeled.max()+1))
+
+    # Converting peaks to lines in image
+    for peak in peaks:
+
+        # Extracting parameters
+        theta = peak[0] - theta_offset
+        rho = peak[1] - rho_offset
+
+        # Converting parameters to row,col
         i0 = rho * np.cos(np.deg2rad(theta))
         j0 = rho * np.sin(np.deg2rad(theta))
 
+        # Finding endpoints for line
         i1 = int(i0 - 1000 * np.sin(np.deg2rad(theta)))
         j1 = int(j0 + 1000 * np.cos(np.deg2rad(theta)))
-
         i2 = int(i0 + 1000 * np.sin(np.deg2rad(theta)))
         j2 = int(j0 - 1000 * np.cos(np.deg2rad(theta)))
 
-        cv2.line(img_copy, (i1, j1), (i2, j2), (0, 0, 255), 2)
+        # Adding red line to image
+        cv2.line(img_copy, (j1, i1), (j2, i2), (0, 0, 255), 2)
 
     return img_copy
-        
-    # Normalize the accumulator matrix and convert to uint8
-    # accum_mat_norm = cv2.normalize(accum_mat, None, 0, 255, cv2.NORM_MINMAX)
-    # accum_mat_uint8 = accum_mat_norm.astype(np.uint8)
-
-    # # Apply histogram equalization
-    # equ = cv2.equalizeHist(accum_mat_uint8)
-
-    # print(np.unique(accum_mat)[::-1])  # Descending order
-
-    # plt.figure()
-    # plt.imshow(img_edges, cmap='gray')
-    # plt.figure()
-    # plt.imshow(accum_mat_norm, cmap='gray', aspect='auto')
-    # plt.show()
--- mp6/mp6_implementation.py
+++ mp6/mp6_implementation.py
@@ -1,33 +1,19 @@
 import cv2
-import numpy as np
 from HoughTransform import HoughTransform
-from CannyEdgeDetection import CannyEdgeDetection
 
 input = cv2.imread('mp6/images/input.bmp')
 test = cv2.imread('mp6/images/test.bmp')
 test2 = cv2.imread('mp6/images/test2.bmp')
 
-input_lines = HoughTransform(input)
-test_lines = HoughTransform(test, percentageOfNonEdge=.94)
-test2_lines = HoughTransform(test2, percentageOfNonEdge=.94)
-
-# joy1_smoothed = GaussSmoothing(joy1, 5, 1)
-# pointer1_smoothed = GaussSmoothing(pointer1, 5, 1)
-# lena_smoothed = GaussSmoothing(lena, 5, 1)
-# test1_smoothed = GaussSmoothing(test1, 5, 1)
-
-# joy1_edges = CannyEdgeDetection(joy1, N=5, sigma=2, percentageOfNonEdge=.8)
-# pointer1_edges = CannyEdgeDetection(pointer1, N=5, sigma=2, percentageOfNonEdge=.8)
-# lena_edges = CannyEdgeDetection(lena, N=5, sigma=1, percentageOfNonEdge=.8)
-# test1_edges = CannyEdgeDetection(test1, N=5, sigma=2, percentageOfNonEdge=.8)
-# test_edges = CannyEdgeDetection(test, N=5, sigma=2, percentageOfNonEdge=.95)
-
+input_lines = HoughTransform(input, param_quant=180, max_filt_N=15, peaks_thresh_mul=.61)
+test_lines = HoughTransform(test, percentageOfNonEdge=.94, param_quant=180, max_filt_N=15, peaks_thresh_mul=.21)
+test2_lines = HoughTransform(test2, percentageOfNonEdge=.94, param_quant=180, max_filt_N=15, peaks_thresh_mul=.22)
 
 cv2.imshow('input.bmp', input)
 cv2.imshow('test.bmp', test)
 cv2.imshow('test2.bmp', test2)
-cv2.imshow('Lines of input.bmp', input_lines)
-cv2.imshow('Lines of test.bmp', test_lines)
-cv2.imshow('Lines of test2.bmp', test2_lines)
+cv2.imshow('Line Detection on input.bmp', input_lines)
+cv2.imshow('Line Detection on test.bmp', test_lines)
+cv2.imshow('Line Detection on test2.bmp', test2_lines)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
